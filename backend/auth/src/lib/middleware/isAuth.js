4b7aea6065eef4005d0cad84ab7c8fdd2a6f2911
Alvarian <alvarivan88@gmail.com>
2022-01-02 19:02:57 -0500

    create middlewares for checking user and password validation

---
+++ backend/auth/src/lib/middleware/isAuth.js
@@ -0,0 +1,37 @@
+const { compare } = require('bcryptjs');
+
+
+module.exports.hasAuth = (req, res, next) => {
+    const { username, password } = req.body;
+
+    const [user] = await User.find({ email: username });
+    
+    if (!user || user.email !== username) {
+        logger.info("Error at login", `User '${username}' does not exist!`);
+        return res.sendStatus(403);
+    }
+
+    compare(password, user.password, async (err, response) => {
+        if (err) {
+            logger.info("Error at login", err);
+            return res.sendStatus(500);
+        }
+
+        if (!response) {
+            logger.info("Error at login", `Password '${password}' is incorrect`);
+            return res.sendStatus(401);
+        }
+
+        req.user = user;
+
+        next();
+    });
+};
+
+module.exports.isUser = (req, res, next) => {
+    const userExists = await User.find({ email: req.body.username });
+    
+    if (userExists.length) return res.sendStatus(200);
+
+    next();
+};
--- backend/auth/src/routes/user-routes.js
+++ backend/auth/src/routes/user-routes.js
@@ -10,57 +10,33 @@ const express = require('express');
 const router = express.Router();
 const { hash, genSalt, compare } = require('bcryptjs');
 const User = require('../models/User');
-const { generateAccessToken, generateRefreshToken } = require('../lib/generateTokens')
+const { generateAccessToken, generateRefreshToken } = require('../lib/generateTokens');
+const { hasAuth } = require('../lib/middleware/isAuth');
 const { redis } = require('../database/config');
 const logger = require('../lib/logger');
 const { createHash } = require('crypto');
 
 
 // post / if login works using body details with password and user from db, cache (new refresh token)() and send (new access token)(). If not send status unauthorized
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    const [user] = await User.find({ email: username });
+router.post('/login', hasAuth, async (req, res) => {
+    const user = req.user;
     
-    if (!user || user.email !== username) {
-        logger.info("Error at login", `User '${username}' does not exist!`)
-        return res.sendStatus(403);
-    }
+    const refreshToken = generateRefreshToken(user.toJSON());
+    const cacheDB = await redis;
+    cacheDB.setEx(user.email, 3600, refreshToken);
 
-    compare(password, user.password, async (err, response) => {
-        if (err) {
-            logger.info("Error at login", err);
-            return res.sendStatus(500);
-        }
-
-        if (!response) {
-            logger.info("Error at login", `Password '${password}' is incorrect`);
-            return res.sendStatus(401);
-        }
-        
-        const refreshToken = generateRefreshToken(user.toJSON());
-        const cacheDB = await redis;
-        cacheDB.setEx(user.email, 3600, refreshToken);
-
-        res.cookie('accessToken', generateAccessToken({user_id: user.userID}), {
-            httpOnly: true, 
-            // secure: true // turn on in prod
-        });
-
-        res.json(res.cookies); // turn off when client is connected
-        // res.sendStatus(200);
+    res.cookie('accessToken', generateAccessToken({user_id: user.userID}), {
+        httpOnly: true, 
+        // secure: true // turn on in prod
     });
+
+    res.json(res.cookies); // turn off when client is connected
+    // res.sendStatus(200);
 });
 
 // post if user does not exist hash password and assign to username in db. After, send status okay
-router.post('/register', async (req, res) => {
+router.post('/register', isUser, async (req, res) => {
     const { username, password, isHost } = req.body;
-    const userExists = await User.find({ email: username });
-    
-    if (userExists.length) {
-        logger.info("Error at register", `User '${userExists[0].email}' already exist!`);
-        return res.sendStatus(403);
-    }
 
     genSalt(10, function(err, salt) {
         if (err) {
